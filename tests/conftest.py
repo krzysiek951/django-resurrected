e5fbda13eae38454f34594a8cb681b884c79a300
Krzysztof Czopkiewicz <krzysiek951@gmail.com>
2025-07-29 22:04:55 +0200

    add / update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 from datetime import datetime
 
 import pytest
-from freezegun import freeze_time
 from model_bakery import baker
 from test_app import models
 
@@ -24,33 +23,31 @@ def assert_is_removed(*objs, removed_at: datetime | None = None):
 
 @pytest.fixture
 def make_authors():
-    def make_authors(quantity: int = 3):
-        authors = baker.make(models.Author, _quantity=quantity)
-        for author in authors:
-            assert_is_active(author)
-        return authors
-
-    return make_authors
+    return lambda quantity=3, **kwargs: baker.make(
+        models.Author, _quantity=quantity, **kwargs
+    )
 
 
 @pytest.fixture
 def make_author_profile():
-    return lambda author: baker.make(models.AuthorProfile, author=author)
+    return lambda author, **kwargs: baker.make(
+        models.AuthorProfile, author=author, **kwargs
+    )
 
 
 @pytest.fixture
 def make_book():
-    return lambda author: baker.make(models.Book, author=author)
+    return lambda author, **kwargs: baker.make(models.Book, author=author, **kwargs)
 
 
 @pytest.fixture
 def make_book_category():
-    return lambda: baker.make(models.BookCategory)
+    return lambda **kwargs: baker.make(models.BookCategory, **kwargs)
 
 
 @pytest.fixture
 def make_book_meta():
-    return lambda book: baker.make(models.BookMeta, book=book)
+    return lambda book, **kwargs: baker.make(models.BookMeta, book=book, **kwargs)
 
 
 @pytest.fixture
@@ -59,44 +56,15 @@ def test_author(make_authors):
 
 
 @pytest.fixture
-@freeze_time("2025-05-01")
-def removed_author(test_author):
-    test_author.remove()
-    assert_is_removed(test_author)
-    return test_author
-
-
-@pytest.fixture
 def author_with_profile(test_author, make_author_profile):
     profile = make_author_profile(author=test_author)
-    assert_is_active(test_author, profile)
     return test_author, profile
 
 
 @pytest.fixture
-@freeze_time("2025-05-01")
-def removed_author_with_removed_profile(author_with_profile):
-    author, profile = author_with_profile
-    author.remove()
-    assert_is_removed(author, profile)
-    return author, profile
-
-
-@pytest.fixture
 def author_with_books(test_author, make_book):
-    book1 = make_book(author=test_author)
-    book2 = make_book(author=test_author)
-    assert_is_active(test_author, book1, book2)
-    return test_author, [book1, book2]
-
-
-@pytest.fixture
-@freeze_time("2025-05-01")
-def removed_author_with_removed_books(author_with_books):
-    author, books = author_with_books
-    author.remove()
-    assert_is_removed(author, *books)
-    return author, books
+    books = make_book(author=test_author, _quantity=2)
+    return test_author, books
 
 
 @pytest.fixture
@@ -104,17 +72,6 @@ def book_with_category(test_author, make_book, make_book_category):
     book = make_book(test_author)
     category = make_book_category()
     book.categories.add(category)
-    assert_is_active(book, category)
-    return book, category
-
-
-@pytest.fixture
-@freeze_time("2025-05-01")
-def removed_book_with_category(book_with_category):
-    book, category = book_with_category
-    book.remove()
-    assert_is_removed(book)
-    assert_is_active(category)
     return book, category
 
 
@@ -127,14 +84,4 @@ def author_with_nested_relations(
     book_meta = make_book_meta(book)
     book.author = author
     book.save()
-    return author, profile, book, book_meta, category
-
-
-@pytest.fixture
-@freeze_time("2025-05-01")
-def removed_author_with_nested_relations(author_with_nested_relations):
-    author, profile, book, book_meta, category = author_with_nested_relations
-    author.remove()
-    assert_is_removed(author, profile, book, book_meta)
-    assert_is_active(category)
     return author, profile, book, book_meta, category
--- tests/test_models.py
+++ tests/test_models.py
@@ -20,22 +20,32 @@ class TestSoftDeleteModel:
         assert isinstance(Author.removed_objects, RemovedObjectsManager)
 
     @freeze_time("2025-05-01")
-    def test_is_expired(self, test_author):
+    def test_is_expired(self, test_author, monkeypatch):
+        assert_is_active(test_author)
         assert test_author.retention_days == 30
         assert test_author.is_expired is False
+
         test_author.remove()
-        test_author.refresh_from_db()
+
+        assert_is_removed(test_author)
         assert test_author.is_expired is False
+
         with freeze_time("2025-05-31"):
             assert test_author.is_expired is False
+
         with freeze_time("2025-06-01"):
             assert test_author.is_expired
 
+            monkeypatch.setattr(Author, "retention_days", None)
+            assert test_author.is_expired is False
+
     @freeze_time("2025-05-01")
     def test_remove(self, test_author):
+        assert_is_active(test_author)
+
         test_author.remove()
-        assert_is_removed(test_author)
-        assert test_author.removed_at == datetime(2025, 5, 1, tzinfo=pytz.utc)
+
+        assert_is_removed(test_author, removed_at=datetime(2025, 5, 1, tzinfo=pytz.utc))
 
     def test_remove_missing_pk(self):
         author = Author()
@@ -44,33 +54,41 @@ class TestSoftDeleteModel:
             match="Operation cannot be performed on Author because its id attribute is "
             "None.",
         ):
-            author.restore()
+            author.remove()
 
     @freeze_time("2025-05-01")
     def test_remove_with_relation_o2o_cascade(self, author_with_profile):
         author, profile = author_with_profile
+        assert_is_active(author, profile)
+
         author.remove()
-        assert_is_removed(profile)
-        assert profile.removed_at == datetime(2025, 5, 1, tzinfo=pytz.utc)
+
+        assert_is_removed(
+            author, profile, removed_at=datetime(2025, 5, 1, tzinfo=pytz.utc)
+        )
 
     @freeze_time("2025-05-01")
     def test_remove_with_relation_m2o_cascade(self, author_with_books):
         author, books = author_with_books
         book1, book2 = books
+        assert_is_active(author, book1, book2)
+
         author.remove()
-        assert_is_removed(author, book1, book2)
-        assert book1.removed_at == datetime(2025, 5, 1, tzinfo=pytz.utc)
+
+        assert_is_removed(
+            author, book1, book2, removed_at=datetime(2025, 5, 1, tzinfo=pytz.utc)
+        )
 
     def test_remove_with_relation_m2m(self, book_with_category):
         book, category = book_with_category
+        assert_is_active(book, category)
+
         book.remove()
+
         assert_is_removed(book)
         # NOTE: M2M relations are not removed, as Django doesn't include them in cascade
         # operations.
         assert_is_active(category)
-
-    def test_remove_with_nested_relations(self, removed_author_with_nested_relations):
-        pass
 
     def test_hard_delete(self, test_author):
         assert Author.objects.filter(id=test_author.id).exists()
@@ -79,9 +97,12 @@ class TestSoftDeleteModel:
 
     @freeze_time("2025-05-01")
     def test_delete(self, test_author):
-        assert Author.objects.filter(id=test_author.id).exists()
+        assert_is_active(test_author)
+
         test_author.delete()
+
         assert_is_removed(test_author)
+
         with freeze_time("2025-06-01"):
             test_author.delete()
             assert Author.objects.filter(id=test_author.id).exists() is False
@@ -93,11 +114,15 @@ class TestSoftDeleteModel:
             match="Operation cannot be performed on Author because its id attribute is "
             "None.",
         ):
-            author.restore()
+            author.delete()
 
-    def test_restore(self, removed_author):
-        removed_author.restore()
-        assert_is_active(removed_author)
+    def test_restore(self, test_author):
+        test_author.remove()
+        assert_is_removed(test_author)
+
+        test_author.restore()
+
+        assert_is_active(test_author)
 
     def test_restore_missing_pk(self):
         author = Author()
@@ -108,38 +133,53 @@ class TestSoftDeleteModel:
         ):
             author.restore()
 
-    def test_restore_without_related_o2o_cascade(
-        self, removed_author_with_removed_profile
-    ):
-        author, profile = removed_author_with_removed_profile
+    def test_restore_without_related_o2o_cascade(self, author_with_profile):
+        author, profile = author_with_profile
+        author.remove()
+        assert_is_removed(author, profile)
+
         author.restore(with_related=False)
+
         assert_is_active(author)
         assert_is_removed(profile)
 
-    def test_restore_with_related_o2o_cascade(
-        self, removed_author_with_removed_profile
-    ):
-        author, profile = removed_author_with_removed_profile
+    def test_restore_with_related_o2o_cascade(self, author_with_profile):
+        author, profile = author_with_profile
+        author.remove()
+        assert_is_removed(author, profile)
+
         author.restore()
+
         assert_is_active(author, profile)
 
-    def test_restore_with_related_m2o_cascade(self, removed_author_with_removed_books):
-        author, books = removed_author_with_removed_books
+    def test_restore_with_related_m2o_cascade(self, author_with_books):
+        author, books = author_with_books
+        author.remove()
+        assert_is_removed(author, *books)
+
         author.restore()
+
         assert_is_active(author, *books)
 
-    def test_restore_with_related_m2m(self, removed_book_with_category):
-        book, book_category = removed_book_with_category
+    def test_restore_with_related_m2m(self, book_with_category):
+        book, book_category = book_with_category
+        book.remove()
         book_category.remove()
+        assert_is_removed(book, book_category)
+
         book.restore()
+
         assert_is_active(book)
         # NOTE: M2M relations are not restored, as Django doesn't include them in
         # cascade operations.
         assert_is_removed(book_category)
 
-    def test_restore_with_nested_relations(self, removed_author_with_nested_relations):
-        author, profile, book, book_meta, category = (
-            removed_author_with_nested_relations
-        )
+    def test_restore_with_nested_relations(self, author_with_nested_relations):
+        author, profile, book, book_meta, category = author_with_nested_relations
+        author.remove()
+        category.remove()
+        assert_is_removed(author, profile, book, book_meta, category)
+
         author.restore()
-        assert_is_active(author, profile, book, book_meta, category)
+        assert_is_active(author, profile, book, book_meta)
+        assert_is_removed(category)
