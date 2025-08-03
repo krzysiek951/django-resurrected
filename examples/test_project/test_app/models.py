61198e6442af2c2bf9b22f2c1d376befb491715a
Krzysztof Czopkiewicz <krzysiek951@gmail.com>
2025-08-03 22:07:12 +0200

    use uuid as primary key in test models to prevent false positives in queryset tests

--- examples/test_project/test_app/models.py
+++ examples/test_project/test_app/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 
 from django_resurrected.models import SoftDeleteModel
@@ -5,28 +7,35 @@ from django_resurrected.models import SoftDeleteModel
 from .constants import BookFormat
 
 
-class Author(SoftDeleteModel):
+class BaseModel(SoftDeleteModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    class Meta:
+        abstract = True
+
+
+class Author(BaseModel):
     name = models.CharField(max_length=100)
 
 
-class AuthorProfile(SoftDeleteModel):
+class AuthorProfile(BaseModel):
     author = models.OneToOneField(
         Author, on_delete=models.CASCADE, related_name="profile"
     )
     bio = models.TextField(blank=True)
 
 
-class BookCategory(SoftDeleteModel):
+class BookCategory(BaseModel):
     name = models.CharField(max_length=100, unique=True)
 
 
-class Book(SoftDeleteModel):
+class Book(BaseModel):
     title = models.CharField(max_length=100)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     categories = models.ManyToManyField(BookCategory, related_name="books", blank=True)
 
 
-class BookMeta(SoftDeleteModel):
+class BookMeta(BaseModel):
     book = models.OneToOneField(Book, on_delete=models.CASCADE, related_name="meta")
     format = models.CharField(
         max_length=30, choices=BookFormat.choices, default=BookFormat.PAPERBACK
